16eb22d9f1d4714b5ce2157f9053becb3bd9d302
MonkeyAndrei <andrejveremcuk712@gmail.com>
2023-06-14 16:51:30 +0300

    add animation in player

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -14,12 +14,12 @@ import { AiOutlineStepForward } from "react-icons/ai";
 import { useAtom } from "jotai";
 import { currentSoundAtom } from "@/atom";
 import Image from "next/image";
-import {getSoundTime} from "@/utils/getSoundTime";
+import { getSoundTime } from "@/utils/getSoundTime";
 
 export const Player: React.FC = () => {
   const [currSound] = useAtom(currentSoundAtom);
   const [isPaused, setIsPaused] = useState<boolean>(false);
-  // const [currentTime, setCurrentTime] = useState<number>(0);
+  const [currentTime, setCurrentTime] = useState<number>(0);
 
   const audioRef: MutableRefObject<HTMLAudioElement | null> = useRef(null);
 
@@ -46,42 +46,72 @@ export const Player: React.FC = () => {
     [currSound]
   );
 
+  const onTimeUpdate = (e: ChangeEvent<HTMLAudioElement>) => {
+    setCurrentTime(e.target.currentTime);
+  };
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }, [currSound]);
+
   if (currSound) {
     return (
-      <div className="w-full h-fit fixed b-[10px] bg-blue-200">
-        <div>{currSound.title}</div>
-
-        <div className="flex justify-between">
-          <div>{getSoundTime(audioRef.current?.currentTime)}</div>
-          <input
-            type="range"
-            max={audioRef.current?.duration}
-            defaultValue={audioRef.current?.currentTime}
-            onChange={onSetTime}
-            className="w-full"
+      <div className="w-full h-fit fixed flex bg-blue-200 bg-opacity-70 bottom-0">
+        <div className="flex w-fit">
+          <Image
+            className={`w-[100px] h-[100px] rounded-full ${
+              isPaused ? "" : "animate-spin-slow"
+            }`}
+            width={100}
+            height={100}
+            alt={currSound.title}
+            src={currSound.posterPath}
           />
-          <div>{getSoundTime(audioRef.current?.duration)}</div>
+          <div>{currSound.title}</div>
         </div>
-        <div className="w-full flex justify-around">
-          <button>
-            <AiOutlineStepBackward size={32} />
-          </button>
-          <>
-            {isPaused ? (
-              <button onClick={onPlayClick}>
-                <FiPlay size={32} />
-              </button>
-            ) : (
-              <button onClick={onPauseClick}>
-                <AiFillPauseCircle size={32} />
-              </button>
-            )}
-          </>
-          <button>
-            <AiOutlineStepForward />
-          </button>
+
+        <div className="w-full">
+          <div className="flex justify-between">
+            <div>{getSoundTime(currentTime)}</div>
+            <input
+              type="range"
+              max={audioRef.current?.duration}
+              value={currentTime}
+              onChange={onSetTime}
+              className="w-full"
+            />
+            <div>{getSoundTime(audioRef.current?.duration)}</div>
+          </div>
+          <div className="w-full flex justify-around">
+            <button>
+              <AiOutlineStepBackward size={32} />
+            </button>
+            <>
+              {isPaused ? (
+                <button onClick={onPlayClick}>
+                  <FiPlay size={32} />
+                </button>
+              ) : (
+                <button onClick={onPauseClick}>
+                  <AiFillPauseCircle size={32} />
+                </button>
+              )}
+            </>
+            <button>
+              <AiOutlineStepForward />
+            </button>
+          </div>
         </div>
-        <audio controls ref={audioRef} src={currSound.musicPath} className="hidden" />
+
+        <audio
+          controls
+          ref={audioRef}
+          src={currSound.musicPath}
+          className="hidden"
+          onTimeUpdate={onTimeUpdate}
+        />
       </div>
     );
   }
