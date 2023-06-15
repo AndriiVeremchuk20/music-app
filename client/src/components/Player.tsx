a62fed1dca4caee19e4241bb95e4ad64d6971a6f
MonkeyAndrei <andrejveremcuk712@gmail.com>
2023-06-15 15:36:22 +0300

    update playlist in player

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,7 +1,6 @@
 import React, {
   ChangeEvent,
   MutableRefObject,
-  RefObject,
   useCallback,
   useEffect,
   useRef,
@@ -16,8 +15,8 @@ import { currentSoundAtom } from "@/atom";
 import Image from "next/image";
 import { getSoundTime } from "@/utils/getSoundTime";
 import { BsFillVolumeOffFill, BsFillVolumeUpFill } from "react-icons/bs";
-import {Playlist} from "./Playlist";
-import {RiPlayListFill} from "react-icons/ri";
+import { Playlist } from "./Playlist";
+import { RiPlayListFill } from "react-icons/ri";
 
 const volumeStep = 10;
 
@@ -71,85 +70,93 @@ export const Player: React.FC = () => {
     }
   }, [volume, setVolume]);
 
+  const onShowPlaylistClick = useCallback(() => {
+    setShowPlaylist((prev) => !prev);
+  }, []);
+
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.play();
       setIsPaused(false);
-	  audioRef.current.volume = volume/100;
+      audioRef.current.volume = volume / 100;
     }
   }, [currSound]);
 
   if (currSound) {
     return (
-      <div className="w-full h-fit fixed flex bg-blue-200 bg-opacity-70 bottom-0 px-4 py-2">
-        <div className="flex flex-col items-center w-fit mr-4">
-          <Image
-            className={`w-[90px] h-[90px] border border-black shadow-xl rounded-full ${
-              isPaused ? "" : "animate-spin-slow"
-            }`}
-            width={100}
-            height={100}
-            alt={currSound.title}
-            src={currSound.posterPath}
-          />
-          <span className="text-sm">{currSound.title}</span>
-        </div>
-
-        <div className="w-full">
-          <div className="flex justify-between mb-5">
-            <div>{getSoundTime(currentTime)}</div>
-            <input
-              type="range"
-              max={audioRef.current?.duration}
-              value={currentTime}
-              onChange={onSetTime}
-              className="w-full mx-1"
+      <div className="w-full h-fit fixed flex flex-col bg-blue-200 bg-opacity-90 bottom-0 px-4 py-2">
+        <div className="w-full h-fit flex">
+          <div className="flex flex-col items-center w-fit mr-4">
+            <Image
+              className={`w-[90px] h-[90px] border border-black shadow-xl rounded-full ${
+                isPaused ? "" : "animate-spin-slow"
+              }`}
+              width={100}
+              height={100}
+              alt={currSound.title}
+              src={currSound.posterPath}
             />
-            <div>{getSoundTime(audioRef.current?.duration)}</div>
+            <span className="text-sm">{currSound.title}</span>
           </div>
-          <div className="w-full flex justify-between">
-            <div className="w-full flex justify-around">
-              <button>
-                <AiOutlineStepBackward size={40} />
-              </button>
-              <>
-                {isPaused ? (
-                  <button onClick={onPlayClick}>
-                    <FiPlay size={32} />
-                  </button>
-                ) : (
-                  <button onClick={onPauseClick}>
-                    <AiFillPauseCircle size={40} />
-                  </button>
-                )}
-              </>
-              <button>
-                <AiOutlineStepForward size={40} />
-              </button>
-            </div>
-            <div className="flex items-center gap-2 mx-2">
-              <button onClick={onVolumeDown}>
-                <BsFillVolumeOffFill size={30} />
-              </button>
-              <span>{volume}%</span>
-              <button onClick={onVolumeUp}>
-                <BsFillVolumeUpFill size={30} />
-              </button>
-            </div>
-			<RiPlayListFill className="mx-4 my-2 cursor-pointer" size={30}/>
-          </div>
-        </div>
 
-        <audio
-          controls
-          ref={audioRef}
-          src={currSound.musicPath}
-          className="hidden"
-          onTimeUpdate={onTimeUpdate}
-        />
-		{
-			showPlaylist? <Playlist/> : null
-		}
+          <div className="w-full">
+            <div className="flex justify-between mb-5">
+              <div>{getSoundTime(currentTime)}</div>
+              <input
+                type="range"
+                max={audioRef.current?.duration}
+                value={currentTime}
+                onChange={onSetTime}
+                className="w-full mx-1"
+              />
+              <div>{getSoundTime(audioRef.current?.duration)}</div>
+            </div>
+            <div className="w-full flex justify-between">
+              <div className="w-full flex justify-around">
+                <button>
+                  <AiOutlineStepBackward size={40} />
+                </button>
+                <>
+                  {isPaused ? (
+                    <button onClick={onPlayClick}>
+                      <FiPlay size={32} />
+                    </button>
+                  ) : (
+                    <button onClick={onPauseClick}>
+                      <AiFillPauseCircle size={40} />
+                    </button>
+                  )}
+                </>
+                <button>
+                  <AiOutlineStepForward size={40} />
+                </button>
+              </div>
+              <div className="flex items-center gap-2 mx-2">
+                <button onClick={onVolumeDown}>
+                  <BsFillVolumeOffFill size={30} />
+                </button>
+                <span>{volume}%</span>
+                <button onClick={onVolumeUp}>
+                  <BsFillVolumeUpFill size={30} />
+                </button>
+              </div>
+              <RiPlayListFill
+                onClick={onShowPlaylistClick}
+                className="mx-4 my-2 cursor-pointer"
+                size={30}
+              />
+            </div>
+          </div>
+
+          <audio
+            controls
+            ref={audioRef}
+            src={currSound.musicPath}
+            className="hidden"
+            onTimeUpdate={onTimeUpdate}
+          />
+        </div>
+        {showPlaylist ? <Playlist /> : null}
       </div>
     );
   }
