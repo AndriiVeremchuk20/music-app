6af3070c2ceba2d21fe18cf05341465a86e696e0
AndriiVeremchuk20 <andrejveremcuk712@gmail.com>
2023-06-23 19:13:37 +0300

    add readme file

--- client/next.config.js
+++ client/next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    FIREBASE_API_KEY: "AIzaSyAi4p8C5JZTM7vWYazw-3CwCNaOBGr9TG0",
-    FIREBASE_AUTH_DOMAIN: "music-app-30d41.firebaseapp.com",
-    FIREBASE_PROJECT_ID: "music-app-30d41",
-    FIREBASE_STORAGE_BUCKET: "music-app-30d41.appspot.com",
-    FIREBASE_MESSAGING_SENDER_ID: "167267173426",
-    FIREBASE_APP_ID: "1:167267173426:web:aa38aa855b1d1b13a7e82b",
-    FIREBASE_MEASUREMENT_ID: "G-M9YKR6DRH0",
-    API_BASE_URL: "http://localhost:9000/",
+    FIREBASE_API_KEY: "",
+    FIREBASE_AUTH_DOMAIN: "",
+    FIREBASE_PROJECT_ID: "",
+    FIREBASE_STORAGE_BUCKET: "",
+    FIREBASE_MESSAGING_SENDER_ID: "",
+    FIREBASE_APP_ID: "",
+    FIREBASE_MEASUREMENT_ID: "",
+    API_BASE_URL: "",
   },
   images: {
     domains: ['music-app-media-data.s3.eu-north-1.amazonaws.com', 'lh3.googleusercontent.com'],
